f0a7e209aa70f8e7e76d0eb94b348be5c73bc47e
Georgiy-huma <georgiy.havrylenko@huma.com>
2023-02-07 14:07:35 +0200

    Setup mailgun
    - Mailgun packages
    - Route for sending emails

---
+++ global.d.ts
@@ -0,0 +1,9 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: string;
+    PORT: string;
+    CORS_ORIGIN: string;
+    MAILGUN_API_KEY: string;
+    MAILGUN_DOMAIN: string;
+  }
+}
--- index.ts
+++ index.ts
@@ -1,21 +1,43 @@
 import express, { Express, Request, Response } from 'express'
-import dotenv from 'dotenv'
 import axios from 'axios'
 import cors from 'cors'
-
-dotenv.config()
+import 'dotenv/config'
+import nodemailer from 'nodemailer'
+import mg from 'nodemailer-mailgun-transport'
 
 const app: Express = express()
 app.use(express.json())
 app.use(cors({credentials: true, origin: process.env.CORS_ORIGIN}))
 const port = process.env.PORT
 
+const auth = {
+  auth: {
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN
+  }
+}
+const nodemailerMailgun = nodemailer.createTransport(mg(auth))
+
+// default route
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server')
+  res.send('jsm-backend')
 })
 
-app.post('/sendmail', (req: Request, res: Response) => {
-  res.send('Sending email...')
+app.post('/sendemail', (req: Request, res: Response) => {
+  nodemailerMailgun.sendMail({
+    from: req.body.email,
+    to: 'gavrilenko.georgi@gmail.com', // An array if you have multiple recipients.
+    subject: 'JSM contact form message',
+    'replyTo': req.body.email,
+    html: `<p>${req.body.message}</p>`,
+    text: req.body.message
+  }, (err, info) => {
+    if (err) {
+      res.send(`Error: ${err.message}`)
+    } else {
+      res.send(`Response: ${info.messageId}`)
+    }
+  })
 })
 
 app.post('/captcha', (req: Request, res: Response) => {
